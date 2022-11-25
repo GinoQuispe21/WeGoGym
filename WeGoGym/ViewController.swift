9ea684bc6918e2af7476df711bd61058aca7ed4e
Gino Salvador Quispe Calixto <gino_21quispe@outlook.com>
2022-11-25 15:24:54 -0500

    create project :tada:

---
+++ WeGoGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeGoGym
+//
+//  Created by Gino Salvador Quispe Calixto on 25/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
