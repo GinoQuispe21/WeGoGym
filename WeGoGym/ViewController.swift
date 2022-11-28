b0a0ad7464653f1b947c93767ec13c2c609b1b65
Gino Salvador Quispe Calixto <gino_21quispe@outlook.com>
2022-11-28 13:25:46 -0500

    :sparkles: added signIn and ssingUp screens

--- WeGoGym/ViewController.swift
+++ WeGoGym/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("HOLA")
     }
-
-
+    @IBAction func signUp(_ sender: UIButton) {
+        print("Sign Up")
+        performSegue(withIdentifier: "signUpSegue", sender: nil)
+    }
+    @IBAction func signIn(_ sender: UIButton) {
+        print("Sign In")
+        performSegue(withIdentifier: "signInSegue", sender: nil)
+    }
 }
 
