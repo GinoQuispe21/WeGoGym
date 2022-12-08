81e13ccdb5cdd9fc6d75db01724f7824d4a7e604
Gino Salvador Quispe Calixto <gino_21quispe@outlook.com>
2022-12-08 02:07:32 -0500

    :sparkles: added feature to add list of exercises into a routine

--- WeGoGym/HomeView/RoutinesView/AddRoutineView/AddRoutineViewController.swift
+++ WeGoGym/HomeView/RoutinesView/AddRoutineView/AddRoutineViewController.swift
@@ -14,12 +14,12 @@ protocol AddRoutineViewControllerDelegate {
 class AddRoutineViewController: UIViewController {
     
     var excercisesAddedArray : [Excercise] = []
-    
-    let allExcercises : [Excercise] = [
+    var backupAllExcercises : [Excercise] = []
+    var allExcercises : [Excercise] = [
         Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
-        Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
-        Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
-        Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
+        Excercise(name: "Sentadillas", description: "bonito ejercicio", muscle: "Piernas", series: 0, reps: 0),
+        Excercise(name: "Dominadas", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
+        Excercise(name: "Press Militar", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
         Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
         Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
         Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
@@ -36,7 +36,6 @@ class AddRoutineViewController: UIViewController {
         Excercise(name: "Press de Banca", description: "bonito ejercicio", muscle: "Pectoral", series: 0, reps: 0),
     ]
 
-    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descTextField: UITextField!
     @IBOutlet weak var searchExcerciseTextField: UITextField!
@@ -45,14 +44,15 @@ class AddRoutineViewController: UIViewController {
     @IBOutlet weak var excercisesTableView: UITableView!
     @IBAction func SaveBarButtonItem(_ sender: UIBarButtonItem) {
         
+        
         let name = nameTextField.text
         let desc = descTextField.text
+        let excercies = excercisesAddedArray
         
-        guard let na = name, let de = desc, !na.isEmpty, !de.isEmpty else {return}
+        guard let na = name, let de = desc, !excercies.isEmpty, !na.isEmpty, !de.isEmpty else {return}
         
         let author = "mi"
         let time = 0
-        let excercies : [Excercise] = []
         let countExcercies = excercies.count
         
         let routine = Routine(name: na, desciption: de, author: author, time: time, excercies: excercies, countExc: countExcercies)
@@ -62,22 +62,51 @@ class AddRoutineViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    var countExcercises : Int = 0
+    
+    @IBOutlet weak var counExcercisesLabel: UILabel!
+    @IBOutlet weak var timeExcercisesLabel: UILabel!
+    
     var delegate : AddRoutineViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        excercisesTableView.dataSource = self
+        backupAllExcercises = allExcercises
+        searchExcerciseTextField.addTarget(self, action: #selector(searchExcerciseTextField(_:)), for: .editingChanged)
         
+        excercisesTableView.dataSource = self
         excercisesAddedCollectionView.dataSource = self
         excercisesAddedCollectionView.delegate = self
         
         nameTextField.placeholder = "Nombre de la Rutina"
         descTextField.placeholder = "Descripción de la rutina"
         searchExcerciseTextField.placeholder = "Buscar un ejercicio por nombre o grupo muscular"
+        counExcercisesLabel.text = "0 ejercicios"
     }
 
 }
+
+// For manage text field to search in table view
+
+extension AddRoutineViewController {
+    @objc func searchExcerciseTextField(_ textField: UITextField){
+        
+        var excercisesFiltered : [Excercise] = []
+        
+        if textField == searchExcerciseTextField {
+            let text = searchExcerciseTextField.text ?? ""
+            allExcercises.forEach({ value in
+                if value.name.lowercased().contains(text.lowercased()) || value.muscle.lowercased().contains(text.lowercased()) {
+                    excercisesFiltered.append(value)
+                }
+            })
+            allExcercises = (text.count > 0) ? excercisesFiltered : backupAllExcercises
+            excercisesTableView.reloadData()
+        }
+    }
+}
+
 
 // CollectionView
 
@@ -93,6 +122,9 @@ extension AddRoutineViewController : UICollectionViewDataSource, UICollectionVie
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "excerciseAddedCollectionViewCell", for: indexPath) as? ExcerciseAddedCollectionViewCell else {return UICollectionViewCell()}
+        cell.delegate = self
+        cell.viewController = self
+        cell.indexCell = indexPath.row
         cell.setupValues(excercise: excercisesAddedArray[indexPath.row])
         return cell
     }
@@ -112,7 +144,29 @@ extension AddRoutineViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "execerciseListTableViewCell", for: indexPath) as? ExecerciseListTableViewCell else {return UITableViewCell()}
         cell.setupValues(excercise: allExcercises[indexPath.row])
+        cell.delegate = self
+        cell.parentViewController = self
         return cell
+    }
+}
+
+// Delegate Cell Table View
+
+extension AddRoutineViewController : ExecerciseListTableViewCellDelegate {
+    func addExcercise(excercise: Excercise) {
+        excercisesAddedArray.append(excercise)
+        excercisesAddedCollectionView.reloadData()
+        counExcercisesLabel.text = "\(excercisesAddedArray.count) ejercicios"
+    }
+}
+
+// Delegate Cell Collection View
+
+extension AddRoutineViewController : ExcerciseRoutineDetailCollectionViewCellDelegate {
+    func excerciseAddedCollectionViewCell(_ collectionViewCell: UICollectionViewCell, didRemoveCell cell: ExcerciseAddedCollectionViewCell, index: Int) {
+        excercisesAddedArray.remove(at: index)
+        excercisesAddedCollectionView.reloadData()
+        counExcercisesLabel.text = "\(excercisesAddedArray.count) ejercicios"
     }
 }
 
@@ -122,7 +176,6 @@ extension UICollectionView {
         let messageLabel = UILabel()
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
         messageLabel.textColor = UIColor.lightGray
-        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 15)
         emptyView.addSubview(messageLabel)
         messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
         messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
--- WeGoGym/HomeView/RoutinesView/AddRoutineView/ExcerciseAddedCollectionViewCell.swift
+++ WeGoGym/HomeView/RoutinesView/AddRoutineView/ExcerciseAddedCollectionViewCell.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
+protocol ExcerciseRoutineDetailCollectionViewCellDelegate : AnyObject {
+    func excerciseAddedCollectionViewCell(_ collectionViewCell: UICollectionViewCell, didRemoveCell cell: ExcerciseAddedCollectionViewCell, index: Int)
+}
+
 class ExcerciseAddedCollectionViewCell: UICollectionViewCell {
     
+    var viewController : UIViewController?
     var excerciseCell : Excercise?
+    var indexCell : Int = 0
+    weak var delegate : ExcerciseRoutineDetailCollectionViewCellDelegate?
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var seriesLabel: UILabel!
     @IBOutlet weak var repsLabel: UILabel!
+    
+    @IBAction func deleteExcerciseButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Eliminar Ejercicio", message: "¿Estás seguro de eliminar esta ejercicio?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: { action in
+        }))
+        alert.addAction(UIAlertAction(title: "Eliminar", style: .destructive, handler: { action in
+            self.delegate?.excerciseAddedCollectionViewCell(self, didRemoveCell: self, index: self.indexCell)
+        }))
+        viewController?.present(alert, animated: true)
+    }
+    
+    @IBAction func updateExcerciseButton(_ sender: UIButton) {
+    }
+    
     
     func setupValues(excercise: Excercise){
         nameLabel.text = excercise.name
